4359cc060ec33df5b6da67a0d980e5a8967a1647
Keith Turner <kturner@apache.org>
2015-04-20 16:57:02 -0400

    make fluo props file an argument

--- config-fluo.sh
+++ config-fluo.sh
@@ -2,12 +2,14 @@
 
 #run this script before initializing fluo
 
+FLUO_PROPS=$1
+
 mvn package
 
 hadoop fs -copyFromLocal target/stress-balancer-0.0.1-SNAPSHOT.jar /fluo/lib
 
-if ! grep -q stress-balancer $FLUO_HOME/conf/fluo.properties
+if ! grep -q stress-balancer $FLUO_PROPS
 then
-  sed -i 's/io.fluo.admin.accumulo.classpath=\(.*\)/io.fluo.admin.accumulo.classpath=\1,\${io.fluo.admin.hdfs.root}\/fluo\/lib\/stress-balancer-0.0.1-SNAPSHOT.jar/' $FLUO_HOME/conf/fluo.properties
+  sed -i 's/io.fluo.admin.accumulo.classpath=\(.*\)/io.fluo.admin.accumulo.classpath=\1,\${io.fluo.admin.hdfs.root}\/fluo\/lib\/stress-balancer-0.0.1-SNAPSHOT.jar/' $FLUO_PROPS
 fi
 
