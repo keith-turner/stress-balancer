5a8d9f281b0e19499145c2122297c4c9611a2141
Keith Turner <kturner@apache.org>
2015-04-20 17:09:57 -0400

    add mkdir -p /fluo/lib

--- config-fluo.sh
+++ config-fluo.sh
@@ -6,10 +6,12 @@ FLUO_PROPS=$1
 
 mvn package
 
+hadoop fs -mkdir -p /fluo/lib
 hadoop fs -copyFromLocal target/stress-balancer-0.0.1-SNAPSHOT.jar /fluo/lib
 
 if ! grep -q stress-balancer $FLUO_PROPS
 then
+  echo "Adding stress balancer to classpath in $FLUO_PROPS"
   sed -i 's/io.fluo.admin.accumulo.classpath=\(.*\)/io.fluo.admin.accumulo.classpath=\1,\${io.fluo.admin.hdfs.root}\/fluo\/lib\/stress-balancer-0.0.1-SNAPSHOT.jar/' $FLUO_PROPS
 fi
 
