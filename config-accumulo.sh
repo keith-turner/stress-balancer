2acf60b742d996365072b4e07106635bf3e708fa
Keith Turner <kturner@apache.org>
2015-04-20 18:20:20 -0400

    pull accumulo config from props

--- config-accumulo.sh
+++ config-accumulo.sh
@@ -2,5 +2,16 @@
 
 #run this after fluo init
 
-accumulo shell -u root -p secret -e 'config -t stress -s table.balancer=accumulo.balancer.StressBalancer'
+FLUO_PROPS=$1
+
+function echo_prop() {
+  prop=$1
+  echo "`grep $prop $FLUO_PROPS | cut -d = -f 2- | grep -v $prop`"
+}
+
+ACCUMULO_USER=`echo_prop io.fluo.client.accumulo.user`
+ACCUMULO_PASS=`echo_prop io.fluo.client.accumulo.password`
+ACCUMULO_TABLE=`echo_prop io.fluo.client.application.name`
+
+accumulo shell -u $ACCUMULO_USER -p $ACCUMULO_PASS -e "config -t $ACCUMULO_TABLE -s table.balancer=accumulo.balancer.StressBalancer"
 
